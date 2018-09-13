62f918c87c5d5bca0a13145e12d68ca9b0fbffce
pqanh09 <pqanh09@gmail.com>
2018-09-13 08:41:01 +0700

    chattingAction

---
+++ springboot/src/main/java/com/example/springboot/command/ChattingActionCmd.java
@@ -0,0 +1,25 @@
+package com.example.springboot.command;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+
+import com.example.springboot.model.Match;
+import com.example.springboot.request.Request;
+import com.example.springboot.response.ResponseType;
+import com.example.springboot.response.UserResponse;
+import com.example.springboot.service.CommonService;
+
+public class ChattingActionCmd extends AbsActionCmd implements ActionCmd {
+	private static final Logger logger = LoggerFactory.getLogger(ChattingActionCmd.class);
+	public ChattingActionCmd(CommonService commonService, SimpMessageSendingOperations simpMessageSendingOperations) {
+		super(commonService, simpMessageSendingOperations);
+	}
+
+	@Override
+	public void execute(Request request, Match match) {
+		String userName = request.getUser();
+		simpMessageSendingOperations.convertAndSend("/topic/"+match.getMatchId()+"/chatting", new UserResponse(ResponseType.Chatting, userName, request.getId()));
+	}
+
+}
