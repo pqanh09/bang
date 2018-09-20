66742d08482790cf3d275a99da05c822aa2f8fb9
pqanh09 <pqanh09@gmail.com>
2018-09-21 06:07:52 +0700

    aaaa

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+   @RequestMapping("/")
+   public String index() {
+      return "index";
+   }
+}
