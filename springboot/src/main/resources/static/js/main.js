6b07e2bded7599a2086738e648ccd2fb1ef4caf2
anhp <pqanh@tma.com.vn>
2018-09-24 17:40:02 +0700

    5392409181

--- springboot/src/main/resources/static/js/main.js
+++ springboot/src/main/resources/static/js/main.js
@@ -768,10 +768,10 @@ myapp.controller('FirstCtrl',
 				$scope.playerDrawingCard = '';
 				$scope.actionType = response.responseType;
 				/// count down time
-//				$interval.cancel(timerSkill);
-//				$scope.timeSkill = response.countDown;
-//				$scope.playerCountDown = response.userName;
-//				timerSkill = $interval(timerSkillFunc, 1000);
+				$interval.cancel(timerSkill);
+				$scope.timeSkill = response.countDown;
+				$scope.playerCountDown = response.userName;
+				timerSkill = $interval(timerSkillFunc, 1000);
 				///
 				$scope.$apply();
 			} else if (response.responseType === 'UseCard') {
